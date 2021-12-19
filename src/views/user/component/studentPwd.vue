c13a1a25226862bb16689d014415ec1163359907
Arman <life.is2short@outlook.com>
2021-12-19 15:09:41 +0800

    管理员和学生修改密码

---
+++ src/views/user/component/studentPwd.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="app-container u-center">
+    <el-form :model="dialogForm">
+      <el-form-item label="用户名" label-width="120px">
+        <el-input v-model="username" disabled style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="学号" label-width="120px">
+        <el-input v-model="studentId" disabled style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="旧密码" label-width="120px">
+        <el-input v-model="dialogForm.oldPassword" style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="新密码" label-width="120px">
+        <el-input v-model="dialogForm.newPassword" type="password" style="width: 300px" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="重复新密码" label-width="120px">
+        <el-input v-model="repeatPwd" style="width: 300px" type="password" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button type="primary" @click="submit">修 改</el-button>
+    </span>
+  </div>
+</template>
+
+<script>
+import { getStudentInfo, changePwd } from '@/api/userCenter'
+import store from '@/store'
+
+export default {
+  data() {
+    return {
+      id: this.$store.state.user.id,
+      role: this.$store.state.user.role,
+      dialogForm: {},
+      username: '',
+      repeatPwd: '',
+      studentId: ''
+    }
+  },
+  created() {
+    this.userInfo()
+  },
+  methods: {
+    async submit() {
+      if (this.repeatPwd !== this.dialogForm.newPassword) {
+        this.$message.error('两次密码不一样')
+        this.dialogForm.newPassword = ''
+        this.repeatPwd = ''
+        return
+      }
+      this.dialogForm.id = this.id
+      this.dialogForm.role = this.role
+      console.log(this.dialogForm)
+      const res = await changePwd(this.dialogForm)
+      if (res.success) {
+        try {
+          await this.$confirm(
+            '密码已修改成功，请重新登录',
+            '系统提示',
+            {
+              confirmButtonText: '重新登录',
+              cancelButtonText: '取消',
+              type: 'warning'
+            })
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        } catch (e) {
+          this.$message.error(e)
+        }
+      }
+    },
+    async userInfo() {
+      const res = await getStudentInfo(this.id)
+      console.log(res)
+      this.username = res.data.name
+      this.studentId = res.data.studentId
+    }
+  }
+}
+</script>
+
+<style scoped>
+.u-center {
+  margin: 50px 600px;
+}
+.dialog-footer  {
+  margin-left: 345px;
+}
+
+el-form-item {
+  margin-right: 20px;
+}
+</style>
--- src/views/user/pwd.vue
+++ src/views/user/pwd.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="app-container">
-    改密码
+  <div>
+    <student-pwd v-if="role === 'STUDENT'" />
+    <manage-pwd v-else />
   </div>
 </template>
 
 <script>
+import managePwd from '@/views/user/component/managePwd'
+import studentPwd from '@/views/user/component/studentPwd'
+
 export default {
+  components: {
+    managePwd,
+    studentPwd
+  },
+  data() {
+    return {
+      role: this.$store.state.user.role
+    }
+  }
 }
 </script>
 
