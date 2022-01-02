131d46a88f0a7db2eac039cb79e5fba369036695
Arman <life.is2short@outlook.com>
2022-01-02 18:06:02 +0800

    首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-    <div class="dashboard-text">role: {{ role }}</div>
-    <div class="dashboard-text">id: {{ id }}</div>
+<!--    <div class="dashboard-text">name: {{ name }}</div>-->
+<!--    <div class="dashboard-text">role: {{ role }}</div>-->
+<!--    <div class="dashboard-text">id: {{ id }}</div>-->
+    <div class="tmp">
+      <h1 class="title">学 生 管 理 平 台</h1>
+    </div>
   </div>
 </template>
 
@@ -30,5 +33,13 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+}
+.tmp {
+  margin: 0 auto;
+}
+.title {
+  text-align: center;
+  //border: 1px solid red;
+  //margin: auto auto;
 }
 </style>
